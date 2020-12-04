afbd927b0c79cae7820eb5339f73a302fb8f995c
Alba126 <70748661+Alba126@users.noreply.github.com>
2020-12-04 03:48:34 +0300

    Delete individual3.py

--- individual3.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    for i in range(1, 10):
-        for j in range(0, 10):
-            for k in range(0, 10):
-                if i + j + k == i * j * k:
-                    print(f"{i}{j}{k}")
